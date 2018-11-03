a85780cf22dc791276c45f0da410f4c683868cf4
Clayton Hart <claytonhart123@yahoo.com>
2018-11-02 18:54:23 -0700

    basic register and login user functionality

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://claytonhart:test123@ds151293.mlab.com:51293/playlist-converter-backend",
+  secretOrKey: "secret1"
+};
--- server.js
+++ server.js
@@ -3,12 +3,35 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
+const users = require("./routes/api/users");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
 app.get("/", (req, res) => res.send("Hello"));
+
+// Use Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5001;
 
